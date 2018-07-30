83121ef7245ac5244e959526e7d1a50d84af9713
JaeChen <470494530@qq.com>
2018-07-30 22:53:43 +0800

    alexnet.py

---
+++ alexnet.py
@@ -0,0 +1,34 @@
+/*
+ * @Author: Jae Chen 
+ * @Date: 2018-07-30 22:39:43 
+ * @Last Modified by: Jae Chen
+ * @Last Modified time: 2018-07-30 22:51:00
+ */
+
+import numpy as np 
+import tensorflow as tf 
+
+def maxpoolLayer():
+    pass
+
+def convLayer():
+    pass
+
+def fcLayer():
+    pass
+
+def dropout():
+    pass
+
+def LNB():
+    pass
+
+class alexNet(object):
+    def __init__(self):
+        pass
+
+    def model(self):
+        pass
+
+    def loadModel(self, sess):
+        pass
---
+++ main.py
@@ -0,0 +1,6 @@
+/*
+ * @Author: Jae Chen 
+ * @Date: 2018-07-30 22:51:21 
+ * @Last Modified by:   Jae Chen 
+ * @Last Modified time: 2018-07-30 22:51:21 
+ */
